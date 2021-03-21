660628479a3e21549d64aeaeded1752f037e0e4f
JTRMinsk <jtrminsk@yahoo.com>
2021-03-21 16:31:14 +0800

    db config and mapping\entity

--- src/main/java/org/salim/mybatis_plus/MybatisPlusApplication.java
+++ src/main/java/org/salim/mybatis_plus/MybatisPlusApplication.java
@@ -1,9 +1,11 @@
 package org.salim.mybatis_plus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(org.salim.mybatis_plus.mapper)//这个是mybatis的数据表
 public class MybatisPlusApplication {
 
     public static void main(String[] args) {
